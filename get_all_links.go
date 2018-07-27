ec5a8f7423454dce505dc658ce566039f31cf143
Ravishdeep SIngh <ravish@campus-018-004.ucdavis.edu>
2018-07-27 10:57:02 -0700

    Initial commit

---
+++ get_all_links.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+    "github.com/PuerkitoBio/goquery"
+)
+
+func linkScrape() {
+    res, err := http.Get("http://jonathanmh.com")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer res.Body.Close()
+
+    doc, err := goquery.NewDocumentFromReader(res.Body)
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    doc.Find("body a").Each(func(index int, item *goquery.Selection) {
+        linkTag := item.Find("a")
+        link, _ := linkTag.Attr("href")
+        linkText := linkTag.Text()
+        fmt.Printf("Link #%d: %s - %s\n", index, linkText, link)
+    })
+}
+
+func main() {
+    linkScrape()
+}
